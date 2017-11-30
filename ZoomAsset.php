e314a5d77d3ba2c4d4e19d8c2747d8fed9a9e3ac
xjflyttp <xjflyttp@gmail.com>
2017-11-30 10:56:34 +0800

    fix basePath

--- ZoomAsset.php
+++ ZoomAsset.php
@@ -12,7 +12,6 @@ class ZoomAsset extends AssetBundle
 {
 
     public $sourcePath = '@bower/jquery-zoom';
-    public $basePath = '@webroot/assets';
     public $js = ['jquery.zoom.js'];
     public $depends = ['yii\web\JqueryAsset'];
 
